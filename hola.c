9519c11d0724db8512fc0cc5a3f7e379c5432afd
Patricio Pérez <patricio.perez@ceinf.cl>
2015-02-12 16:36:05 -0300

    Indentar ejemplo en C

--- hola.c
+++ hola.c
@@ -1,20 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-		int numero;
-	char cadena[255];
-	printf("Ingrese un numero:\n");
-	scanf("%d",&numero);
-	printf("ingrese su nombre:\n");
-	fflush(stdin); 
-	scanf("%s",cadena);
-	if(numero%2==0)
-	{
-		printf("%s ingresaste un valor par\n", cadena);
-	}else{
-		printf("%s ingresaste un valor impar\n",cadena);
-	}
-	return 0;
+    int numero;
+    char cadena[255];
+    printf("Ingrese un numero:\n");
+    scanf("%d", &numero);
+    printf("ingrese su nombre:\n");
+    fflush(stdin);
+    scanf("%s", cadena);
+    if(numero % 2 == 0) {
+        printf("%s, ingresaste un valor par\n", cadena);
+    } else {
+        printf("%s, ingresaste un valor impar\n", cadena);
+    }
+    return 0;
 }
